74020353e71011fb2535dd2e490067336378fb89
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-09-11 09:29:46 +0200

    Divisions

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Divisions program:");
+
+        int tal1 = 4;
+        int tal2 = 5;
+        System.out.println("Ange tal 1: "+tal1);
+        System.out.println("Ange tal 2: "+tal2);
+        System.out.println("Kvoten är: "+((double)tal1/tal2));
+        System.out.println("Kvoten är: "+(tal1/tal2));
+    }
+}
