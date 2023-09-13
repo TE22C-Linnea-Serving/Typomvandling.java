7450d898ec3ffbf1852373a31e65be492cb33452
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-09-13 10:45:11 +0200

    Substantiv och pluraländelse

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
@@ -9,5 +11,14 @@ public class App {
         System.out.println("Ange tal 2: "+tal2);
         System.out.println("Kvoten är: "+((double)tal1/tal2));
         System.out.println("Kvoten är: "+(tal1/tal2));
+
+        Scanner tangentbord = new Scanner(System.in);
+        System.out.println();
+
+        System.out.println("Ange ett substantiv: ");
+        String substantiv = tangentbord.nextLine();   
+        System.out.println("Ange substantivets pluraländelse: ");
+        String pluraländelse = tangentbord.nextLine();
+        System.out.println("En "+substantiv+", flera "+substantiv+pluraländelse);
     }
 }
